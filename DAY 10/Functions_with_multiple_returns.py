35634e3653aa83a9e5892968f086baf440b3cce8
Kushagra-73 <kushagra.official.73@gmail.com>
2025-07-05 16:48:11 +0530

    mains

--- DAY 10/Functions_with_multiple_returns.py
+++ DAY 10/Functions_with_multiple_returns.py
@@ -1,6 +1,16 @@
 # Functions with multiple return statements
 # Functions terminate at the return keyword. If you write code below the return statement that code will not be executed.
 
+"""
+| Code                                  | What it does                                      |
+| ------------------------------------- | ------------------------------------------------- |
+| `functions[operation]`                | Fetches the function (like `add`)                 |
+| `functions[operation](x, y)`          | **Calls** the function with arguments `x` and `y` |
+| `result = functions[operation]`       | Stores the function itself, not the result        |
+| `result = functions[operation](x, y)` | Stores the result of running the function         |
+"""
+
+
 def format_name(f_name,l_name):
     formated_f_name=f_name.title()
     formated_l_name=l_name.title()
